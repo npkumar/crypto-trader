cbeec7ada43e3e9389eb86d8a67aef302887a9bb
Nitin Pradeep Kumar <getnpk@gmail.com>
2017-12-24 17:21:14 +0800

    Fill up CryptoCoin component

--- src/components/cryptocoins/CryptoCoins.vue
+++ src/components/cryptocoins/CryptoCoins.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Cryptocoins - Cryptocoins Component</h1>  
-</template>
+  <div>
+    <crypto-coin v-for="coin in cryptoCoins">
+
+    </crypto-coin>
+  </div>
+</template> 
+
+<script>
+import CryptoCoin from './CryptoCoin.vue'
+export default {
+  data () {
+    return {
+      cryptoCoins: [
+        { id: 1, name: 'Bitcoin', price: 11000 },
+        { id: 2, name: 'Ethereum', price: 1100 },
+        { id: 3, name: 'IOTA', price: 11 },
+        { id: 4, name: 'Litecoin', price: 500 }
+      ]
+    }
+  },
+  components: {
+    CryptoCoin
+  }
+}
+</script>
