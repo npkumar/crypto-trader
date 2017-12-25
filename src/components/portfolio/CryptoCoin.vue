9beb3f906e903969c0b233770d0b4658f8ca7fd3
Nitin Pradeep Kumar <getnpk@gmail.com>
2017-12-25 07:42:17 -0500

    Fix sellCoin infinite loop

--- src/components/portfolio/CryptoCoin.vue
+++ src/components/portfolio/CryptoCoin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
+    <div class="panel panel-danger">
       <div class="panel-heading">
         <h3 class="panel-title">
           {{ coin.name }}
@@ -17,7 +17,7 @@
         </div>
         <div class="pull-right">
           <button
-            class="btn btn-success"
+            class="btn btn-danger"
             @click="sellCoin"
             :disabled="quantity <= 0 || !!Number.isInteger(quantity)"
             >Sell</button>
@@ -37,17 +37,18 @@ export default {
       quantity: 0
     }
   },
-  methods: {
-    ...mapActions([
-      'sellCoin'
-    ]),
+  methods: { 
+    ...mapActions({
+      placeSellOrder: 'sellCoin'
+    }),
     sellCoin() {
       const order = {
         coinId: this.coin.id,
         coinPrice: this.coin.price,
         quantity: this.quantity
       };
-      this.sellCoin(order);
+      this.placeSellOrder(order);
+      this.quantity = 0;
     }
   }
 }
