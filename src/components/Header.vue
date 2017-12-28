64013bbc94d3879eb94bf1a0633a36986b2515aa
Nitin Pradeep Kumar <getnpk@gmail.com>
2017-12-28 09:03:44 +0800

    Add funds to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,6 +11,9 @@
           <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
           <router-link to="/cryptocoins" activeClass="active" tag="li"><a>Crypto Coins</a></router-link>
         </ul>
+
+        <strong class="navbar-text navbar-right">Funds: {{ funds }}</strong>
+
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#">End Day</a></li>
           <li class="dropdown">
@@ -32,3 +35,13 @@
     </div><!-- /.container-fluid -->
   </nav>
 </template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+}
+</script>
