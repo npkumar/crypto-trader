3432f44e9f03611310e3d351b60f7b3b5c82c93d
Nitin Pradeep Kumar <getnpk@gmail.com>
2017-12-31 07:48:58 +0800

    Set up vue-resource and http root to firebase

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App'
 import { routes } from './routes'
 
 import store from "./store/store";
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://firebase-project-name.firebaseio.com/';
 
 Vue.filter('currency', value => {
   return `$ ${value.toLocaleString()}`;
